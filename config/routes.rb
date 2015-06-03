f725397c92f3988352ac1831f4040ce31751dcee
Eric Sipple <saalon@gmail.com>
2015-06-03 15:38:03 -0400

    fixing devise routes, which were being initialized twice.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: "home#index"
-
-  devise_for :users, controllers: { sessions: 'sessions' }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
