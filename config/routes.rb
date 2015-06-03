bd4bd982233e1f6f9bb5609237913f76b418dbf2
Eric Sipple <saalon@gmail.com>
2015-06-03 14:31:14 -0400

    Setting Devise up to work with ember-cli-simple-auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: "home#index"
 
+  devise_for :users, controllers: { sessions: 'sessions' }
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
---
+++ test/controllers/sessions_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SessionsControllerControllerTest < ActionController::TestCase
+  def test_sanity
+    flunk "Need real tests"
+  end
+end
