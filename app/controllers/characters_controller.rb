8babb35ab08b94d8138102572f1b2cf6681ec932
Eric Sipple <saalon@gmail.com>
2015-06-23 09:37:25 -0400

    Ammending.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,7 +3,7 @@ class CharactersController < ApplicationController
   before_filter :login_required
 
   def index
-    render text: "Fuck you" unless user_signed_in?
+    render text: "This is a test." unless user_signed_in?
   end
 
 end
